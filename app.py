adf7b9b73f53320ac0145daea56c16cbd84892cc
Joanne <joannesayswu@gmail.com>
2019-06-19 12:14:51 -0700

    added comments

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
-# Put your app in here.
 from flask import Flask, request
 from operations import add, sub, mult, div
 app = Flask(__name__)
 
+# Part 1: Create 4 separate routes to add, subtract, multiply, and divide two numbers
 @app.route('/add')
 def return_sum():
+    """Add a and b parameters"""
     a = int(request.args["a"])
     b = int(request.args["b"])
     return f"<body>{add(a,b)}</body>"
 
 @app.route('/sub')
 def return_dif():
+    """Subtract a and b parameters"""
     a = int(request.args["a"])
     b = int(request.args["b"])
     return f"<body>{sub(a,b)}</body>"
 
 @app.route('/mult')
 def return_product():
+    """Multiply a and b parameters"""
     a = int(request.args["a"])
     b = int(request.args["b"])
     return f"<body>{mult(a,b)}</body>"
 
 @app.route('/div')
 def return_quotient():
+    """Divide a and b parameters"""
     a = int(request.args["a"])
     b = int(request.args["b"])
     return f"<body>{div(a,b)}</body>"
 
+# Part 2: Create one route to execute requested operation?
+
+# Dictionary to map requested operation with the corresponding function from operations.py
 math_operations = {"add": add,
                    "sub": sub,
                    "mult": mult,
@@ -37,7 +44,6 @@ def calculate(operation):
     a = int(request.args["a"])
     b = int(request.args["b"])
 
-    #result = math_operations[operation](a,b) 
     function = math_operations.get(operation)
 
     if(not function):
